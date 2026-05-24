03492c49ea3d4029c98690c6659d4478c8b7d5d9
MiguelTFD <MiguelTFD@outlook.com>
2026-05-24 00:24:51 -0500

    feat: set the codebase path

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import {VueQueryPlugin} from '@tanstack/vue-query';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import App from './App.vue';
+import './style.css';
 
 const app = createApp(App);
+
+app.use(createPinia());
+app.use(VueQueryPlugin);
 app.use(PrimeVue, {
   theme: {
     preset: Aura
   }
 });
 
-app.mount('#app')
+app.mount('#app');
